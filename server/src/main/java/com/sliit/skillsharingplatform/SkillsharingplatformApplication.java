082337ea916f87c2fe30d9577777300db47d3c53
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2025-04-10 17:32:12 +0530

    Initial Commit

---
+++ server/src/main/java/com/sliit/skillsharingplatform/SkillsharingplatformApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.skillsharingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsharingplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsharingplatformApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/sliit/skillsharingplatform/SkillsharingplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.skillsharingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsharingplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
